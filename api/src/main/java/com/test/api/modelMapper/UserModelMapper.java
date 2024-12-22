b19c9ae0a1ecc191b48e3812c9338f68ecd97218
jwujesq8 <zhukovskaja.elena@gmail.com>
2024-12-22 15:58:09 +0300

    fixed modelMapper and its config file, adjusted addUser for a new modelMapper

--- api/src/main/java/com/test/api/modelMapper/UserModelMapper.java
+++ api/src/main/java/com/test/api/modelMapper/UserModelMapper.java
@@ -5,98 +5,35 @@ import com.test.api.dto.request.POSTUserRequestDto;
 import com.test.api.dto.request.PUTUserRequestDto;
 import com.test.api.dto.request.UserRequestDto;
 import com.test.api.dto.response.UserResponseDto;
-import com.test.api.exception.ServerException;
+import com.test.api.repository.GenderRepository;
+import com.test.api.user.Gender;
 import com.test.api.user.User;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.Converter;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeMap;
+import org.modelmapper.spi.MappingContext;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
 
 @Component
 @RequiredArgsConstructor
 public class UserModelMapper {
 
-    protected final ModelMapper mapper;
+    private final ModelMapper mapper;
 
-    public UserModelMapper() {
-        this.mapper = new ModelMapper();
+    public User map_POSTUserRequestDto_to_User(POSTUserRequestDto postUserRequestDto) {
+        return mapper.map(postUserRequestDto, User.class);
     }
 
-    public User convert_UserRequestDto_to_User(UserRequestDto userRequestDto){
-        try{
-            return mapper.map(userRequestDto, User.class);
-        }
-        catch (IllegalArgumentException e) {
-            throw new ServerException.ModelMappingException("Invalid argument provided for mapping: " + e.getMessage());
-        }
-        catch (org.modelmapper.MappingException e) {
-            throw new ServerException.ModelMappingException("Mapping failed: " + e.getMessage());
-        }
-        catch (Exception e) {
-            throw new RuntimeException("Unexpected error during mapping: " + e.getMessage());
-        }
+    public User map_PUTUserRequestDto_to_User(PUTUserRequestDto putUserRequestDto) {
+        return mapper.map(putUserRequestDto, User.class);
     }
 
-    public User convert_POSTUserRequestDto_to_User(POSTUserRequestDto postUserRequestDto){
-        try{
-            return mapper.map(postUserRequestDto, User.class);
-        }
-        catch (IllegalArgumentException e) {
-            throw new ServerException.ModelMappingException("Invalid argument provided for mapping: " + e.getMessage());
-        }
-        catch (org.modelmapper.MappingException e) {
-            throw new ServerException.ModelMappingException("Mapping failed: " + e.getMessage());
-        }
-        catch (Exception e) {
-            throw new RuntimeException("Unexpected error during mapping: " + e.getMessage());
-        }
+    public UserResponseDto map_User_to_UserResponseDto(User user) {
+        return mapper.map(user, UserResponseDto.class);
     }
 
-    public User convert_PUTUserRequestDto_to_User(PUTUserRequestDto putUserRequestDto){
-        try{
-            return mapper.map(putUserRequestDto, User.class);
-        }
-        catch (IllegalArgumentException e) {
-            throw new ServerException.ModelMappingException("Invalid argument provided for mapping: " + e.getMessage());
-        }
-        catch (org.modelmapper.MappingException e) {
-            throw new ServerException.ModelMappingException("Mapping failed: " + e.getMessage());
-        }
-        catch (Exception e) {
-            throw new RuntimeException("Unexpected error during mapping: " + e.getMessage());
-        }
-    }
-
-
-
-
-
-    public UserResponseDto convert_User_to_UserResponseDto(User user){
-        try{
-            return mapper.map(user, UserResponseDto.class);
-        }
-        catch (IllegalArgumentException e) {
-            throw new ServerException.ModelMappingException("Invalid argument provided for mapping: " + e.getMessage());
-        }
-        catch (org.modelmapper.MappingException e) {
-            throw new ServerException.ModelMappingException("Mapping failed: " + e.getMessage());
-        }
-        catch (Exception e) {
-            throw new RuntimeException("Unexpected error during mapping: " + e.getMessage());
-        }
-    }
-
-    public UserRequestDto convert_User_to_UserRequestDto(User user){
-        try{
-            return mapper.map(user, UserRequestDto.class);
-        }
-        catch (IllegalArgumentException e) {
-            throw new ServerException.ModelMappingException("Invalid argument provided for mapping: " + e.getMessage());
-        }
-        catch (org.modelmapper.MappingException e) {
-            throw new ServerException.ModelMappingException("Mapping failed: " + e.getMessage());
-        }
-        catch (Exception e) {
-            throw new RuntimeException("Unexpected error during mapping: " + e.getMessage());
-        }
-    }
 }
